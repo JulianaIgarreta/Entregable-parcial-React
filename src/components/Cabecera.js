bc8bc678190ff1477e3b1326a027dc34e8e986b4
JulianaIgarreta <igarretajuliana@gmail.com>
2022-08-31 23:42:29 -0300

    Proyecto parcial terminado

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import '../index.css'
 
-// El componente Cabera no tiene componentes hijos.
-// ESTADO: Cabecera no tiene estado.
-// MÉTODOS: Cabecera no tiene métodos.
-// PROPS: Cabecera recibe de su padre la cantidad que va a mostrar dentro del span correpondiente
-// Maqueta de Cabecera:
-//    h1
-//    p > span     (el span mostrará la cantidad recibida por props)
 
 export default function Cabecera({elementos}) {
   
